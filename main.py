fe0872bc53ea542fc8f79f70eb5d2e8d9d982fd0
nc.phu <phunc.aiengineer@gmail.com>
2025-02-11 21:47:36 +0700

    add .env file for environment variables and load it in main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from backend.src.routers import upload
+
+load_dotenv()
 
 app = FastAPI()
 
